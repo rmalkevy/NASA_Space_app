c58e1de42747cd4672fefc313b971c8ee58d0088
Konstantin Shyshkin <k.shyshkin@cleverdec.com>
2018-10-20 18:00:39 +0300

    next try

--- backend/queries/urls.py
+++ backend/queries/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
 
 from . import views
-from .floors import getUsersByFloor, getImageFloor, getMaps
-from .charts import getChartsInfoDaily, getChartsInfoRange
-from .correlation import getCorrelations
-from .forecasting import forecasting
+from .data import getData
 
 app_name = 'queries'
 
 urlpatterns = [
-    path('getMaps/', getMaps.as_view()),
-    path('getUsersByFloor/', getUsersByFloor.as_view()),
-    path('getImageFloor/', getImageFloor.as_view()),
-    path('getChartsInfoDaily/', getChartsInfoDaily.as_view()),
-    path('getChartsInfoRange/', getChartsInfoRange.as_view()),
-    path('getCorrelations/', getCorrelations.as_view()),
-    path('forecasting/', forecasting.as_view())
+    path('getData/', getData.as_view()),
 ]
