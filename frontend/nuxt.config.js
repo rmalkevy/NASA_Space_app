a241f8d192784467a3bf03b46b27aa8ccac3ef74
rmalkevy <henprokuror@gmail.com>
2018-10-20 19:01:16 +0300

    before HTML

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -38,6 +38,7 @@ module.exports = {
   */
   plugins: [
     '@/plugins/vuetify',
+    '@/plugins/heatmap'
   ],
 
   /*
---
+++ frontend/plugins/heatmap.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Heatmap from 'vue2-leaflet'
+
+Vue.use(Heatmap);
